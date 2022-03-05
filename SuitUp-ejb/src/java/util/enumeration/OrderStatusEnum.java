12cc79a0983b4695e73ef50ba4167e62e64940df
keithcharlesc <keithcharleschan@gmail.com>
2022-03-06 00:45:58 +0800

    Enums added: AppointmentTypeEnum, CollectionMethodEunm, OrderStatusEnum

---
+++ SuitUp-ejb/src/java/util/enumeration/OrderStatusEnum.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util.enumeration;
+
+/**
+ *
+ * @author keithcharleschan
+ */
+public enum OrderStatusEnum {
+    UNPAID,
+    PAID,
+    READY_FOR_PICKUP,
+    PICKED_UP,
+    IN_DELIVERY,
+    DELIVERED,
+    COMPLETED,
+    CANCELLED
+}
