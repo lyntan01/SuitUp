1657ee460d2be472d470df1bd50012202af66496
keithcharlesc <keithcharleschan@gmail.com>
2022-04-17 19:46:42 +0800

    Merge origin/main into main
    Conflicts:
    	SuitUp-ejb/src/java/util/security/GlassFishCryptographicHelper.java
    	SuitUp-war/web/WEB-INF/glassfish-web.xml
    	SuitUp-war/web/WEB-INF/web.xml

--- SuitUp-ejb/src/java/util/security/GlassFishCryptographicHelper.java
+++ SuitUp-ejb/src/java/util/security/GlassFishCryptographicHelper.java
@@ -17,7 +17,7 @@ public class GlassFishCryptographicHelper
     private static final String GLASSFISH_DEFAULT_DOMAIN_CONFIG_DIRECTORY = "/Users/keithcharleschan/glassfish-5.1.0/glassfish/domains/domain1/config";
 //    private static final String GLASSFISH_DEFAULT_DOMAIN_CONFIG_DIRECTORY = "/Users/lyntan/glassfish-5.1.0/glassfish/domains/domain1/config";
     // private static final String GLASSFISH_DEFAULT_DOMAIN_CONFIG_DIRECTORY = "/Users/meganyee/glassfish-5.1.0/glassfish/domains/domain1/config";
-    private static final String GLASSFISH_DEFAULT_DOMAIN_CONFIG_DIRECTORY = "/Users/xianhui/glassfish-5.1.0/glassfish/domains/domain1/config";
+//    private static final String GLASSFISH_DEFAULT_DOMAIN_CONFIG_DIRECTORY = "/Users/xianhui/glassfish-5.1.0/glassfish/domains/domain1/config";
 
     private static final String GLASSFISH_DEFAULT_KEYSTORE_NAME = "keystore.jks";
     private static final String GLASSFISH_DEFAULT_KEYSTORE_PATH = GLASSFISH_DEFAULT_DOMAIN_CONFIG_DIRECTORY + System.getProperty("file.separator") + GLASSFISH_DEFAULT_KEYSTORE_NAME;
